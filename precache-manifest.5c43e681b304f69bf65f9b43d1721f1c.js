633f92cffda843faa4e6c5a076303ba6370432a1
Tanya <fullstackon@gmail.com>
2020-06-18 17:50:40 +0300

    Updates

---
+++ precache-manifest.5c43e681b304f69bf65f9b43d1721f1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abd6e00d4e4452de74c12c1cdd1e67d",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "634806b0c4284973cea0",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.854a80aa.chunk.css"
+  },
+  {
+    "revision": "ed5f9ffc2359b20c1406",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.aa958f66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.aa958f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "634806b0c4284973cea0",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.75e7cb6a.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
